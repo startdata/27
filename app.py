81407af944b9f75a443b08403ec160bc321a6bb2
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-09-14 15:48:15 +0900

    posting movie url method

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import datetime
 import hashlib
 import jwt
+import requests
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 from flask import Flask, render_template, jsonify, request
@@ -11,6 +13,10 @@ client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 SECRET_KEY = "SPARTA"
+
+# Web Crawling
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 # route
 
@@ -119,6 +125,39 @@ def api_login():
 @app.route('/api/logout', methods=['POST'])
 def api_logout():
     return jsonify({'result': "success", 'msg': '로그아웃되었습니다'})
+
+
+@app.route('/api/url', methods=['POST'])
+def apiPosting():
+    # 현재 접속중인 사람이 누군지 알기 위해서 토큰 복호화
+    token = request.cookies.get('mytoken')
+    payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    user = db.users.find_one({"id": payload['id']})
+    # 스크래핑할 url 받아오기
+    url = request.form['url']
+
+    # 여기서 스크래핑
+    data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    title = soup.select_one(
+        '#content > div.article > div.mv_info_area > div.mv_info > h3 > a:nth-child(1)').text
+
+    # db.title collection에 저장하기
+    posting = {
+        "title": title,
+        "url": url,
+        "like": 0,
+        "dislike": 0,
+        "owner": user["_id"]
+    }
+    result = db.postings.insert_one(posting)
+    # print(result.inserted_id)
+
+    # user에도 저장하기
+    db.users.update_one(user, {"$push": {'posts': result.inserted_id}})
+    # user = db.users.find_one_and_update(
+    #     {'id': payload['id']}, {"$push": {'posts': url}})
+    return jsonify({'result': "success", 'msg': '포스팅 완료!'})
 
 
 if __name__ == '__main__':
